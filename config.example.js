89da38ea14559b1a1906d7ba1a9e76a6535bc538
Kevin Gao <kevin.gao@datafox.co>
2017-01-26 13:36:14 -0800

    Update config example

--- config.example.js
+++ config.example.js
@@ -5,13 +5,13 @@ const CONFIG = {
     // Left Screen (Macbook)
     {key: '4', width: 1280, height: 800,  x: -1280, y: 0},
     // Main Screen (Full, Vertical Halves, Quadrants)
-    {key: 'f', width: 2560, height: 1573, x: 0,     y: 23},
-    {key: '5', width: 1280, height: 1573, x: 0,     y: 23},
-    {key: '6', width: 1280, height: 1573, x: 1280,  y: 23},
-    {key: 'g', width: 1280, height: 786,  x: 0,     y: 23},
-    {key: 'd', width: 1280, height: 786,  x: 0,     y: 810},
-    {key: 'j', width: 1280, height: 786,  x: 1280,  y: 23},
-    {key: 'h', width: 1280, height: 787,  x: 1280,  y: 810},
+    {key: 'f', width: 2560, height: 1578, x: 0,     y: 22},
+    {key: '5', width: 1280, height: 1578, x: 0,     y: 22},
+    {key: '6', width: 1280, height: 1578, x: 1280,  y: 22},
+    {key: 'g', width: 1280, height: 789,  x: 0,     y: 22},
+    {key: 'd', width: 1280, height: 789,  x: 0,     y: 811},
+    {key: 'j', width: 1280, height: 789,  x: 1280,  y: 22},
+    {key: 'h', width: 1280, height: 789,  x: 1280,  y: 811},
     // Right Screen (Full, Horizontal Halves)
     {key: '7', width: 1200, height: 1600, x: 2560,  y: 0},
     {key: 'l', width: 1200, height: 800,  x: 2560,  y: 0},
@@ -22,9 +22,9 @@ const CONFIG = {
     // Left Screen
     {key: 'a', width: 1080, height: 1920, x: -1080, y: 0},
     // Main Screen (Full, Vertical Halves)
-    {key: 't', width: 1920, height: 1053, x: 0,     y: 23},
-    {key: 'c', width: 960,  height: 1053, x: 0,     y: 23},
-    {key: 'v', width: 960,  height: 1053, x: 960,   y: 23},
+    {key: 't', width: 1920, height: 1058, x: 0,     y: 22},
+    {key: 'c', width: 960,  height: 1058, x: 0,     y: 22},
+    {key: 'v', width: 960,  height: 1058, x: 960,   y: 22},
     // Right Screen
     {key: 'o', width: 1440, height: 990,  x: 1920,  y: 0},
   ],
